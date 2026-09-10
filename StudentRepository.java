39d049c6debaa571d0167e34621f9ce94315dc00
krxiee0 <zyquxie@gmail.com>
2026-09-11 07:07:37 +0800

    Add methods to find Student by email and studentId

--- StudentRepository.java
+++ StudentRepository.java
@@ -2,11 +2,14 @@ package com.memorylink.repository;
 
 import com.memorylink.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
     Optional<Student> findByUsername(String username);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
-    boolean existsByStudentId(String studentId);
+
+    Optional<Student> findByEmail(String email);
+
+    Optional<Student> findByStudentId(String studentId);
 }
