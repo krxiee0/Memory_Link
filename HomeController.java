075a55d2d483b46d11a5fed81d72e4416daa0f11
krxiee0 <zyquxie@gmail.com>
2026-09-11 06:38:41 +0800

    Delete HomeController.java
    Remove HomeController class and its associated methods.

--- HomeController.java
+++ HomeController.java
@@ -1,13 +1,1 @@
-package com.memorylink.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-}
