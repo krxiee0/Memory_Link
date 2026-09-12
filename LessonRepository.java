61052caf7c0dc706702c449c8698a4b328fed225
krxiee0 <zyquxie@gmail.com>
2026-09-13 05:18:38 +0800

    Format LessonRepository.java for better readability

--- LessonRepository.java
+++ LessonRepository.java
@@ -1,1 +1,8 @@
-package com.memorylink.repository; import com.memorylink.model.Lesson; import org.springframework.data.jpa.repository.JpaRepository; public interface LessonRepository extends JpaRepository<Lesson,Long>{}
+package com.memorylink.repository;
+
+import com.memorylink.model.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository
+        extends JpaRepository<Lesson, Long> {
+}
