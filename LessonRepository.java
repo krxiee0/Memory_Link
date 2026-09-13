cf3a82b7f37f1c9aa267ec504e38370bfcf5e6b8
krxiee0 <zyquxie@gmail.com>
2026-09-13 20:13:00 +0800

    Update LessonRepository.java

--- LessonRepository.java
+++ LessonRepository.java
@@ -3,6 +3,5 @@ package com.memorylink.repository;
 import com.memorylink.model.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LessonRepository
-        extends JpaRepository<Lesson, Long> {
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
 }
